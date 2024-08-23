dfab67fd04e38a1694415170eee853dbc4fb1233
Sandiakun <lemaru002@gmail.com>
2024-08-22 18:42:18 -0600

    add try

--- Excepciones.py
+++ Excepciones.py
@@ -1,1 +1,11 @@
-print("hola mundo")
+#print("1/0")
+
+numero_var = "hola"
+try:
+    print (int(numero_var))
+
+except Exception as error:
+    print (f"el valor recivido no es entero: {error}")
+
+
+print ("hola mundo")
